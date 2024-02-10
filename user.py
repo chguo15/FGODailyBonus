d34739bc7bd98e116227b48e925f08ae76511556
Wang Jinxu <42631016+Tsuasahi@users.noreply.github.com>
2024-02-10 19:50:33 +0800

    Update user.py

--- user.py
+++ user.py
@@ -96,21 +96,22 @@ class user:
         # 登陆天数
         login_days = data['cache']['updated']['userLogin'][0]['seqLoginCount']
         total_days = data['cache']['updated']['userLogin'][0]['totalLoginCount']
-        res = f'*{self.name_}*\n`登陆天数: {login_days}天 / {total_days}天\n'
+#        res = f'*{self.name_}*\n`登陆天数: {login_days}天 / {total_days}天\n'
+        res = f'登陆天数: {login_days}天 / {total_days}天\n'
 
         # 角色信息
-        res += f'等级: {lv}\n石头: {stone}\n呼符: {ticket}\n'
+        res += f'等级: {lv} 石头: {stone} 呼符: {ticket}\n'
 
         # 现有体力
         act_max = data['cache']['replaced']['userGame'][0]['actMax']
         act_recover_at = data['cache']['replaced']['userGame'][0]['actRecoverAt']
         now_act = (act_max - (act_recover_at - mytime.GetTimeStamp()) / 300)
-        res += f'体力: {now_act} / {act_max}\n'
+#        res += f'体力: {now_act} / {act_max}\n'
 
         # 友情点
         add_fp = data['response'][0]['success']['addFriendPoint']
         total_fp = data['cache']['replaced']['tblUserGame'][0]['friendPoint']
-        res += f'友情点: {add_fp} / {total_fp}`\n'
+#        res += f'友情点: {add_fp} / {total_fp}`\n'
 
         # 登陆奖励
         if 'seqLoginBonus' in data['response'][0]['success']:
